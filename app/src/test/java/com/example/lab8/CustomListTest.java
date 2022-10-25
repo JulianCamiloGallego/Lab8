6fb538ced8041b891ff27bf57938fe41e6e09d3f
Julian Gallego Franco <jcgallego22@gmail.com>
2022-10-25 17:39:06 -0600

    Adding add city test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -21,7 +21,7 @@ public class CustomListTest {
      * get the size of the list
      * increase the list by adding a new city
      * check if our current size matches the initial size plus
-     one
+     one, something
      */
     @Test
     public void addCityTest(){
